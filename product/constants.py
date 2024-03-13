4a00890b869377e9aa8e69ff50041865d5b270ef
viacheslav <vyacheslav.idz@gmail.com>
2024-03-13 19:05:26 +0200

    update prev commit

--- product/constants.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-product_DIR = Path(__file__).parent
-PRODUCTS_FILE = product_DIR / "products.json"
-
-BASE_URL = "https://www.mcdonalds.com"
-RELATIVE_URL = "/ua/uk-ua/eat/fullmenu.html"
